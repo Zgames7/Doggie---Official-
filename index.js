6649ef08fc616901c3466faed977f2474e00c152
Zgames7 <163663519+Zgames7@users.noreply.github.com>
2025-06-28 09:43:03 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === 'ping') {
+    message.reply('pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
